b555a28aef279295299494fed6ba8a5c7434878c
kirestein <erik.proenca2011@gmail.com>
2024-11-14 15:29:51 -0300

    fix: type error in EmployeePage component

--- app/dashboard/employees/[id]/page.tsx
+++ app/dashboard/employees/[id]/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// import { Container } from './styles';
-
 const EmployeePage = ({ param: { id } }: { param: { id: string } }) => {
     return (
         <div className="">
